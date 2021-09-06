d908343b2bf11120b3788b3c5fd7ab9aa7bfe066
Thyago Mota <thyagomota@gmail.com>
2021-09-06 10:22:56 -0600

    updates

---
+++ activity_09_weight_height_2/src/weight_height.py
@@ -0,0 +1,18 @@
+# CS390Z - Introduction to Data Minining - Fall 2021
+# Instructor: Thyago Mota
+# Description: Activity 09 - Visualization
+
+import os
+import csv
+import matplotlib.pyplot as plt 
+
+# definitions/parameters
+DATA_FOLDER = os.path.join('..', 'data')
+FILE_NAME   = 'weight_height.csv'
+
+if __name__ == "__main__":
+
+    # TODO: produce a histograms of female weights with 10 bins starting at 53 and with 15 width
+
+    # TODO: produce a scatter plot of male heights vs. weights 
+
