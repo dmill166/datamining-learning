9e3a1e95554e31005e4512ba859310505186269e
Dakota Miller <dmill166@msudenver.edu>
2021-09-26 17:09:12 -0600

    Updating remaining files

--- activity_11_denver_neighborhoods/src/scraper.py
+++ activity_11_denver_neighborhoods/src/scraper.py
@@ -6,32 +6,35 @@
 # Researched why previous path searching was failing. Identified difference thanks to Guido
 # https://mail.python.org/pipermail/python-dev/2010-February/097461.html
 
-import csv 
-import re 
+import csv
+import re
 import os
 import requests
 from bs4 import BeautifulSoup
 
 # definitions/parameters
 original_path = os.getcwd()
-os.chdir(os.path.dirname((__file__)))
+os.chdir(os.path.dirname(__file__))
 os.chdir('../')
 DATA_FOLDER = os.path.join(os.getcwd(), 'data')
 CSV_FILE_NAME = 'denver_neighborhoods.csv'
 CSV_FILE_PATH = os.path.join(DATA_FOLDER, CSV_FILE_NAME)
 BASE_URL = 'https://www.5280.com/neighborhoods/'
-HEADERS = {"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"}
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"}
 
-def remove_tags(s): 
+
+def remove_tags(s):
     tag = re.compile('<.*?>')
     return re.sub(tag, '', s)
+
 
 # TODO: finish the scraper
 if __name__ == "__main__":
 
     with open(os.path.join(DATA_FOLDER, CSV_FILE_NAME), 'wt') as csv_file:
         writer = csv.writer(csv_file)
-        writer.writerow(['name','population','home_price','schools_score','crime_rank','x_factor'])
+        writer.writerow(['name', 'population', 'home_price', 'schools_score', 'crime_rank', 'x_factor'])
         content = requests.get(BASE_URL, headers=HEADERS).content
         soup = BeautifulSoup(content, 'html.parser')
         trs = soup.find_all('tr')
--- activity_11_denver_neighborhoods/src/similarity.py
+++ activity_11_denver_neighborhoods/src/similarity.py
@@ -2,11 +2,11 @@
 # Instructor: Thyago Mota
 # Description: Activity 11: similarity analysis of neighborhoods in the Denver metro area
 
-import csv 
-import re 
+import csv
+import re
 import os
 import math
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 from matplotlib import cm as cm
 
 # definitions/parameters
@@ -17,14 +17,19 @@ DATA_FOLDER = os.path.join(os.getcwd(), 'data')
 CSV_FILE_NAME = 'denver_neighborhoods.csv'
 CSV_FILE_PATH = os.path.join(DATA_FOLDER, CSV_FILE_NAME)
 
-def min_max(data, mins, maxs, interval=(0,1)):
-    return [ int(((data[i] - mins[i]) / (maxs[i] - mins[i]) * (interval[1] - interval[0]) + interval[0]) * 100000) / 100000 for i in range(len(data))]
 
-def eucl_dist(a, b): 
+def min_max(data, mins, maxs, interval=(0, 1)):
+    return [
+        int(((data[i] - mins[i]) / (maxs[i] - mins[i]) * (interval[1] - interval[0]) + interval[0]) * 100000) / 100000
+        for i in range(len(data))]
+
+
+def eucl_dist(a, b):
     sum = 0
     for i in range(len(a)):
-        sum += (a[i] - b[i])**2
+        sum += (a[i] - b[i]) ** 2
     return int((1 - math.sqrt(sum / len(a))) * 100000) / 100000
+
 
 # TODO: finish the similarity analysis
 if __name__ == "__main__":
@@ -41,7 +46,7 @@ if __name__ == "__main__":
             row[0] = re.sub('Washington', 'Was.', row[0])
             row[0] = re.sub('South', 'S.', row[0])
             neighborhoods.append(row[0])
-            data = [ int(row[1]), int(row[2]), float(row[3]), int(row[4]), float(row[5])]
+            data = [int(row[1]), int(row[2]), float(row[3]), int(row[4]), float(row[5])]
             if row_count == 2:
                 mins = list(data)
                 maxs = list(data)
@@ -52,23 +57,22 @@ if __name__ == "__main__":
             matrix.append(data)
     # print(mins)
     # print(maxs)
-    matrix = [ min_max(data, mins, maxs) for data in matrix ]
+    matrix = [min_max(data, mins, maxs) for data in matrix]
     # print(matrix)
-    
 
     print(matrix[0])
     print(matrix[1])
     print(eucl_dist(matrix[0], matrix[0]))
     print(eucl_dist(matrix[1], matrix[0]))
     print(eucl_dist(matrix[2], matrix[0]))
-    
+
     dm = []
     for i in range(len(matrix)):
         row = []
         for j in range(len(matrix)):
             row.append(eucl_dist(matrix[i], matrix[j]))
         dm.append(row)
-    plt.rc('font', size=8) 
+    plt.rc('font', size=8)
     cmap = cm.get_cmap('YlOrBr')
     img = plt.matshow(dm, cmap=cmap)
     axes = plt.gca()
@@ -76,9 +80,8 @@ if __name__ == "__main__":
     axes.set_yticklabels(neighborhoods)
     axes.set_xticks(list(range(len(neighborhoods))))
     axes.set_xticklabels(neighborhoods, rotation=90)
-    fig = plt.gcf() # reference to the plot
+    fig = plt.gcf()  # reference to the plot
     ticks = [x / 10 for x in range(1, 11)]
     fig.colorbar(img, ticks=ticks)
     # fig.subplots_adjust(top=0.2)
     plt.show()
-
