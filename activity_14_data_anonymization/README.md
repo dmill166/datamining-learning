aaa3df01ac1729af196318c557023d45591e4aed
Thyago Mota <thyagomota@gmail.com>
2021-09-27 16:30:48 -0600

    updates

---
+++ activity_14_data_anonymization/README.md
@@ -0,0 +1,30 @@
+# Activity 14
+
+## Data Anonymization
+
+## Goal
+In this activity you are given a dataset of 1K employees. Because the collection contains personally identifiable information, your task is to apply data anonymization techniques as a pre-process procedure. 
+
+## Instructions
+
+Create a Jupyter notebook with the following sections: 
+
+### Preamble
+
+Title: *HR Data Anonymization*
+
+Author: *\<your name\>*
+
+Email: *\<your email\>*
+
+Last Update: *\<date\>*
+
+### Dataset
+
+Create metadata information describing the attributes of your dataset. 
+
+### Data Anonymization
+
+Identify the attributes that contain personal information. Devise a method to anonymize each of the attributes identified in this section.  
+
+Once you have completed the data anonymization, save the new dataset in a csv file. 
---
+++ activity_14_data_anonymization/src/data_anonymization.py
@@ -0,0 +1,73 @@
+# CS390Z - Introduction to Data Minining - Fall 2021
+# Instructor: Thyago Mota
+# Description: Activity 14: data anonymization
+
+import os
+import pandas as pd
+import random
+import requests
+import time
+from google.colab import drive
+from datetime import datetime, timedelta
+
+# definitions/parameters
+DATA_FOLDER     = '/content/drive/MyDrive/Colab Datasets/human_resources'
+CSV_FILE_NAME   = 'employees.csv'
+ANON_CSV_FILE_NAME = 'employees_anon.csv'
+PSEUDO_NAME_API = 'https://api.namefake.com/'
+
+# Google drive mount
+drive.mount('/content/drive')
+
+class IdGenerator: 
+
+  def __init__(self, min, max, total):
+    self.min = min
+    self.max = max 
+    self.total = total
+
+  def __iter__(self):
+    self.ids = random.sample(range(self.min, self.max + 1), self.total)
+    self.current = 0
+    return self
+
+  def __next__(self):
+    if self.current < len(self.ids):
+      value = self.ids[self.current]
+      self.current += 1 
+      return value
+    else:
+      raise StopIteration 
+
+def get_pseudo_name_email(gender):
+  while True:
+    result = requests.get(PSEUDO_NAME_API)
+    if result.status_code == 200:
+      data = result.json()
+      if 'female' in data['pict']:
+        if gender == 'M':
+          continue
+      else:
+        if gender == 'F':
+          continue
+      name = data['name']
+      email = data['email_u'] + '@' + data['email_d']
+      return name, email
+    else:
+      time.sleep(1)
+
+def shift_date_days(date, days):
+  shift_days = random.randrange(days * 2 + 1) - 30
+  td = timedelta(days=abs(shift_days))
+  if shift_days >= 0:
+    return date + td
+  else:
+    return date - td
+
+def shift_salary_amount(salary, min, max):
+  return salary + random.randrange(2 * (max-min) + 1) - (max-min)
+
+# TODO; finish the data anonymization
+if __name__ == "__main__":
+
+  
