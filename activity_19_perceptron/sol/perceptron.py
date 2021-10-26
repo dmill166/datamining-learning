aeb2eb5a0d490c6b3b47fd6dcbb22722bceeca7f
Thyago Mota <thyagomota@gmail.com>
2021-10-26 15:26:53 -0600

    updates

---
+++ activity_19_perceptron/sol/perceptron.py
@@ -0,0 +1,39 @@
+# CS390Z - Introduction to Data Minining - Fall 2021
+# Instructor: Thyago Mota
+# Description: Activity 19: AND/OR Perceptrons 
+
+from sklearn.linear_model import Perceptron
+import numpy as np
+
+if __name__ == "__main__":
+
+    and_dataset = [ 
+        [1, -1, -1, -1], 
+        [1, -1, 1, -1],
+        [1, 1, -1, -1],
+        [1, 1, 1, 1]
+    ]
+    array = np.array(and_dataset)
+
+    or_dataset = [ 
+        [1, -1, -1, -1], 
+        [1, -1, 1, 1],
+        [1, 1, -1, 1],
+        [1, 1, 1, 1]
+    ]
+
+    xor_dataset = [ 
+        [1, -1, -1, -1], 
+        [1, -1, 1, 1],
+        [1, 1, -1, 1],
+        [1, 1, 1, -1]
+    ]
+
+    array = np.array(xor_dataset)
+
+    # TODO: train a perceptron to implement AND/OR classifiers
+    X = array[:,:-1]
+    Y = array[:,-1]
+    clf = Perceptron(random_state=0).fit(X, Y)
+    print('Weights:', clf.coef_)
+    print('Accuracy:', clf.score(X, Y))
