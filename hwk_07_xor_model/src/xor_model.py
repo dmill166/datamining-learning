6e5db0b1b2e5493cccacd3a6d0fa0b087fd81fbf
Thyago Mota <thyagomota@gmail.com>
2021-11-09 10:32:46 -0700

    udpates

--- hwk_07_xor_model/src/xor_model.py
+++ hwk_07_xor_model/src/xor_model.py
@@ -9,59 +9,72 @@ if __name__ == "__main__":
 
     xor_dataset = [ 
         [1, -1, -1, -1], 
-        [1, -1, 1, 1],
         [1, 1, -1, 1],
+        [1, -1, 1, 1],
         [1, 1, 1, -1]
     ]
     array = np.array(xor_dataset)
 
-    # TODO: train a MLP Classifier to implement a XOR classifier with the minimum number of hidden layers and nodes possible
-    
+    # TODO: train a MLP Classifier to implement a XOR classifier with only 1 hidden layer with 2 nodes
+    X = array[0:,:-1]
+    Y = array[0:,-1]
 
-'''
-x_0 = 1 (always)
-x_1 = -1
-x_2 = -1
-out = ? 
+    # TODO: get the weights (coefs_ attribute)
 
-h_1 = x_0w_01 + x_1w_11 + x_2w_21 
-h_2 = x_0w_02 + x_1w_12 + x_2w_22
+    # TODO: get the biases (intercepts_ attribute)
 
-out = h_1w_1out + h_2w_2out 
-'''
+    # TODO: print the score
 
-'''
-x_0 = 1 (always)
-x_1 = -1
-x_2 = 1
-out = ? 
+    # check the output
+    w_01 = weights[0][0][0] 
+    print("w_01:", w_01)
+    w_02 = weights[0][0][1]
+    print("w_02:", w_02)
+    w_11 = weights[0][1][0]
+    print("w_11:", w_11)
+    w_12 = weights[0][1][1]
+    print("w_12:", w_12)
+    w_21 = weights[0][2][0]
+    print("w_21:", w_21)
+    w_22 = weights[0][2][1]
+    print("w_22:", w_22)
+    w_1out = weights[1][0][0]
+    print("w_1out:", w_1out)
+    w_2out = weights[1][1][0]
+    print("w_2out:", w_2out)
 
-h_1 = x_0w_01 + x_1w_11 + x_2w_21 
-h_2 = x_0w_02 + x_1w_12 + x_2w_22
+    b_1 = biases[0][0]
+    print("b_1:", b_1)
+    b_2 = biases[0][1]
+    print("b_2:", b_2)
+    b_3 = biases[1][0]
+    print("b_3:", b_3)
 
-out = h_1w_1out + h_2w_2out 
-'''
+    x_0 = 1
+    x_1 = -1 # FALSE
+    x_2 = -1 # FALSE
+    h_1 = max(x_0*w_01 + x_1*w_11 + x_2*w_21 + b_1, 0)
+    h_2 = max(x_0*w_02 + x_1*w_12 + x_2*w_22 + b_2, 0)
+    out = h_1*w_1out + h_2*w_2out + b_3
+    print("x_1:", x_1, ", x_2:", x_2, ", out:", out)  
 
-'''
-x_0 = 1 (always)
-x_1 = 1
-x_2 = -1
-out = ? 
+    x_1 = -1 # FALSE
+    x_2 = 1 # TRUE
+    h_1 = max(x_0*w_01 + x_1*w_11 + x_2*w_21 + b_1, 0)
+    h_2 = max(x_0*w_02 + x_1*w_12 + x_2*w_22 + b_2, 0)
+    out = h_1*w_1out + h_2*w_2out + b_3
+    print("x_1:", x_1, ", x_2:", x_2, ", out:", out)  
 
-h_1 = x_0w_01 + x_1w_11 + x_2w_21 
-h_2 = x_0w_02 + x_1w_12 + x_2w_22
+    x_1 = 1 # TRUE
+    x_2 = -1 # FALSE
+    h_1 = max(x_0*w_01 + x_1*w_11 + x_2*w_21 + b_1, 0)
+    h_2 = max(x_0*w_02 + x_1*w_12 + x_2*w_22 + b_2, 0)
+    out = h_1*w_1out + h_2*w_2out + b_3
+    print("x_1:", x_1, ", x_2:", x_2, ", out:", out)  
 
-out = h_1w_1out + h_2w_2out 
-'''
-
-'''
-x_0 = 1 (always)
-x_1 = 1
-x_2 = 1
-out = ?
-
-h_1 = x_0w_01 + x_1w_11 + x_2w_21 
-h_2 = x_0w_02 + x_1w_12 + x_2w_22
-
-out = h_1w_1out + h_2w_2out 
-'''
+    x_1 = 1 # TRUE
+    x_2 = 1 # TRUE
+    h_1 = max(x_0*w_01 + x_1*w_11 + x_2*w_21 + b_1, 0)
+    h_2 = max(x_0*w_02 + x_1*w_12 + x_2*w_22 + b_2, 0)
+    out = h_1*w_1out + h_2*w_2out + b_3
+    print("x_1:", x_1, ", x_2:", x_2, ", out:", out)  
